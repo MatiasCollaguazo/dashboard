48def6cade2546ede0f0504c6b5d38c08badc905
MatiasCollaguazo <mcollagu@espol.edu.ec>
2024-11-27 20:01:18 -0500

    Act

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,97 @@
+{/* Hooks */ }
+import { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import IndicatorWeather from './components/IndicatorWeather';
 import ControlWeather from './components/ControlWeather';
 import TableWeather from './components/TableWeather';
 import LineChartWeather from './components/LineChartWeather';
 
+interface Indicator {
+ title?: String;
+ subtitle?: String;
+ value?: String;
+}
+
 function App() {
+{/* Variable de estado y función de actualización */}
+let [indicators, setIndicators] = useState<Indicator[]>([])
+let [owm, setOWM] = useState(localStorage.getItem("openWeatherMap"))
+
+  {/* Hook: useEffect */}
+  useEffect(()=>{
+
+    let request = async () => {
+
+       {/* Request */}
+       let API_KEY = "b14f773a299d15808f31b452ca782b74"
+       let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+       let savedTextXML = await response.text();
+
+      {/* XML Parser */}
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(savedTextXML, "application/xml");
+
+      {/* Arreglo para agregar los resultados */}
+
+      let dataToIndicators : Indicator[] = new Array<Indicator>();
+
+      {/* 
+         Análisis, extracción y almacenamiento del contenido del XML 
+         en el arreglo de resultados
+      */}
+
+      let name = xml.getElementsByTagName("name")[0].innerHTML || ""
+      dataToIndicators.push({"title":"Location", "subtitle": "City", "value": name})
+
+      let location = xml.getElementsByTagName("location")[1]
+
+      let latitude = location.getAttribute("latitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Latitude", "value": latitude })
+
+      let longitude = location.getAttribute("longitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Longitude", "value": longitude })
+
+      let altitude = location.getAttribute("altitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Altitude", "value": altitude })
+
+      //console.log( dataToIndicators )
+      setIndicators(dataToIndicators)
+    }
+
+    request();
+
+  },[])
+
+let renderIndicators = () => {
+    return (
+        <Grid container spacing={3} style={{ marginBottom: "20px" }}>
+          {indicators.map((indicator, idx) => (
+                <Grid 
+                    item 
+                    key={idx} 
+                    xs={12}  // Ocupa el 100% en pantallas pequeñas
+                    sm={6}   // Ocupa el 50% en pantallas medianas
+                    md={4}   // Ocupa el 33.33% en pantallas grandes
+                    lg={3}   // Ocupa el 25% en pantallas extra grandes
+                >
+                    <IndicatorWeather 
+                        title={indicator["title"]} 
+                        subtitle={indicator["subtitle"]} 
+                        value={indicator["value"]} 
+                    />
+                </Grid>
+            ))}
+        </Grid>
+    );
+};
+
+
+
+  {/*JSX*/}
   return (
-    <Grid container spacing={5}>
+    <Grid container spacing={0}>
       {/* Indicadores */}
+      {/*
       <Grid item xs={12} sm={6} md={3}>
         <IndicatorWeather title="Indicator 1" subtitle="Unidad 1" value="1.23" />
       </Grid>
@@ -20,6 +104,9 @@ function App() {
       <Grid item xs={12} sm={6} md={3}>
         <IndicatorWeather title="Indicator 4" subtitle="Unidad 4" value="3.21" />
       </Grid>
+      */}
+
+       {renderIndicators()}
 
       {/* Tabla */}
       <Grid item xs={12} md={6}>
--- src/components/ControlWeather.tsx
+++ src/components/ControlWeather.tsx
@@ -1,12 +1,23 @@
+ {/* Hooks */ }
+ import { useState, useRef } from 'react';
+
+{/* Interfaz SelectChangeEvent */}
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 
 export default function ControlWeather() {
+
+  {/* Variable de estado y función de actualización */}
+  let [selected, setSelected] = useState(-1)
+
+  {/* Constante de referencia a un elemento HTML */ }
+  const descriptionRef = useRef<HTMLDivElement>(null);
+
   const items = [
     { name: 'Precipitación', description: 'Cantidad de agua que cae sobre una superficie en un período específico.' },
     { name: 'Humedad', description: 'Cantidad de vapor de agua presente en el aire, generalmente expresada como un porcentaje.' },
@@ -19,7 +30,24 @@ export default function ControlWeather() {
     </MenuItem>
   ));
 
+
+   {/* Manejador de eventos */}
+   const handleChange = (event: SelectChangeEvent) => {
+
+       let idx = parseInt(event.target.value)
+       // alert( idx );
+       setSelected( idx );
+
+       {/* Modificación de la referencia descriptionRef */}
+       if (descriptionRef.current !== null) {
+           descriptionRef.current.innerHTML = (idx >= 0) ? items[idx]["description"] : ""
+       }
+   };
+
+
+ {/* JSX */} 
   return (
+     
     <Paper
       sx={{
         p: 2,
@@ -34,7 +62,7 @@ export default function ControlWeather() {
       <Box sx={{ minWidth: 120 }}>
         <FormControl fullWidth>
           <InputLabel id="simple-select-label">Variables</InputLabel>
-          <Select labelId="simple-select-label" id="simple-select" label="Variables" defaultValue="-1">
+          <Select labelId="simple-select-label" id="simple-select" label="Variables" defaultValue="-1" onChange={handleChange}>
             <MenuItem key="-1" value="-1" disabled>
               Seleccione una variable
             </MenuItem>
@@ -42,6 +70,17 @@ export default function ControlWeather() {
           </Select>
         </FormControl>
       </Box>
+
+
+     {/* Use la variable de estado para renderizar del item seleccionado */}
+     {/*<Typography mt={2} component="p" color="text.secondary">
+     {
+         (selected >= 0)?items[selected]["description"]:""
+     }
+     </Typography>*/}
+
+     <Typography ref={descriptionRef} mt={2} component="p" color="text.secondary" />
+
     </Paper>
   );
 }
